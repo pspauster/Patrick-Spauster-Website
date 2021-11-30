4210fd96b7d36396ba56e26844bb83b568d1d078
pspauster <patrick.spauster@gmail.com>
2021-11-30 01:20:11 -0500

    import from enterprise

---
+++ js/app.js
@@ -0,0 +1,355 @@
+
+//Portfolio Carousel
+//Initiate array with data
+
+var portfolio = [
+    {
+        href: "/Docs/MyResume.pdf",
+        image: "images/resume.jpg",
+        title: "My CV",
+        description: "Read about my professional experience. Current to June 2021",
+    },
+    {
+        href: "dataviz.html",
+        image: "images/featured/bubbles.png",
+        title: "My Data Viz Gallery",
+        description: "View some of my published and unpublished data visualizations",
+    },
+    {
+        href: "https://www.urban.org/research/publication/rise-market-reliant-affordable-housing-tools",
+        image: "images/iz.png",
+        title: "The Rise of Market-Reliant Affordable Housing Tools",
+        description: "Findings from the National Longitudinal Land Use Survey",
+    },
+    {
+        href: "https://www.urban.org/features/housing-first-breaks-homelessness-jail-cycle",
+        image: "images/denver-sib-header-50.jpg",
+        title: "Housing First Breaks the Homelessness-Jail Cycle",
+        description: "Results from a life-changing 5 year supportive housing program",
+    },
+    {
+        href: "https://reenvisioning-rural-america.urban.org/",
+        image: "images/ruralfeature.jpg",
+        title: "Reenvisioning Rural America",
+        description: "Our data tool characterizes rural communities by their assets, not their deficiencies",
+    },
+    {
+        href: "https://www.urban.org/research/publication/protecting-housing-choice-voucher-holders-discrimination-lessons-oregon-and-texas",
+        image: "images/vouchers-graph.png",
+        title: "Voucher Protections",
+        description: "Why Oregon chose to protect voucher holders and Texas turned them away"
+    },
+    {
+        href: "https://www.urban.org/research/publication/state-and-local-voucher-protection-laws-introducing-new-legal-dataset",
+        image: "images/vouchersmap-300x185.png",
+        title: "Voucher Protection Laws",
+        description: "Adding new data to the debate on voucher protections nationwide"
+    },
+    {
+        href: "https://www.urban.org/research/publication/housing-and-land-use-implications-split-roll-property-tax-reform-california",
+        image: "images/split-roll.jpg",
+        title: "California's Split Roll Initiative",
+        description: "Split Roll tax reform won't make housing worse in California"
+    },
+    {
+        href: "https://www.urban.org/research/publication/breaking-homelessness-jail-cycle-housing-first-results-denver-supportive-housing-social-impact-bond-initiative",
+        image: "images/denver-300x200.jpg",
+        title: "Denver Supportive Housing RCT",
+        description: "Evidence that supportive housing works",
+    },
+    {
+        href: "https://www.urban.org/research/publication/investing-equitable-urban-park-systems",
+        image: "images/parks-300x141.png",
+        title: "Investing in Equitable Parks",
+        description: "New strategies to build parks where they're most needed"
+    },
+    {
+        href: "https://www.urban.org/research/publication/incorporating-two-generation-approaches-community-change",
+        image: "images/gettyimages-594831019_crop-300x159.jpg",
+        title: "2Gen Programs in Context",
+        description: "What does it take to make real community change?"
+    },
+    {
+        href: "https://www.urban.org/research/publication/promise-heights-response-challenges-created-covid-19",
+        image: "images/PHPN.JPG", 
+        title: "Promise Heights Promise Neighborhood",
+        description: "How did service providers respond to COVID-19?"
+    },
+    {
+        href: "https://www.urban.org/research/publication/developing-two-generation-approaches-communities",
+        image: "images/FCCCcities.JPG",
+        title: "2Gen Community Change", 
+        description: "7 years of research in San Antonio, Columbus, and Buffalo"
+    },
+    {
+        href: "https://www.neighborworks.org/NFMCCapstoneReport",
+        image: "images/housing-crisis_0.jpg",
+        title: "National Foreclosure Crisis",
+        description: "How the nation's biggest foreclosure mitigation program got it done"
+    },
+    {
+        href: "https://www.urban.org/research/publication/evaluation-resident-opportunity-and-self-sufficiency-service-coordinator-program",
+        image: "images/ross.JPG", 
+        title: "Service Coordinator Evaluation",
+        description: "Our report to HUD on a new service model for Public Housing Authorities"
+    },
+    {
+        href: "https://eua2018.protectingeducation.org/#attacks-on-schools",
+        image: "images/eua18gcpea-300x164.jpg",
+        title: "Education Under Attack",
+        description: "I wrote 9 country profiles when I worked at HRW"
+    },
+    {
+        href: "https://www.hrw.org/news/2016/09/20/human-rights-watch-submission-cedaw-committee",
+        image: "images/HRW.png",
+        title: "Women's Rights in Bangladesh",
+        description: "I wrote the 2018 treaty body submission to the UN from HRW"
+    },
+    {
+        href: "http://patrickspauster.com/wp-content/uploads/2017/10/CT-Economy-Srivastava.pdf",
+        image: "images/ct-graphic-300x300.png",
+        title: "The Connecticut Economy",
+        description: "I contributed researh to the economic report"
+    },
+    {
+        href: "/Docs/Spauster_Capstone_PublicPolicy.pdf",
+        image: "images/propera_oportunidades_sedesol2_red-150x150.jpg",
+        title: "Undergraduate Capstone",
+        description: "Comparing approaches to Conditional Cash Transfer Programs in Mexico and Brazil"
+    }
+]
+
+//Writing Carousel
+//Initiate array with data
+var writing = [
+    {
+        href: "https://wp.nyu.edu/wagnerplanner/2021/11/09/ingrid-gould-ellen-reflects/",
+        image: "images/Ingrid2019.jpg",
+        title: "Profile of Ingrid Gould Ellen",
+        description: "On a Successful Career, NYC, Social Media, and the Next Generation Working on Housing Policy",
+    },
+    {
+        href: "https://citylimits.org/2021/09/30/opinion-new-york-city-needs-basement-apartments-now-more-than-ever/",
+        image: "images/Ida.jpg",
+        title: "NYC Needs Basement Apartments",
+        description: "After Ida, we need to legalize basement apartments",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/lebron-jamess-i-promise-school-puts-public-face-evidence-based-approach-whole-family-intervention",
+        image: "images/lebron_header-300x140.jpg",
+        title: "Lebron's school helps the whole family",
+        description: "Lebron's foundation uses the evidence at the I Promise School",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/lebron-jamess-foundation-building-transitional-housing-i-promise-school-it-can-go-further-help-families",
+        image: "images/gettyimages-1007909642_crop-300x140.jpg",
+        title: "Lebron is Building Transitional Housing",
+        description: "But his foundation can do more to help families",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/some-new-york-city-districts-nearly-one-four-students-experience-homelessness",
+        image: "images/nycsd_homelessness-01.png",
+        title: "1 in 10 NYPS students experience homelessness",
+        description: "In some neighborhoods as many as 1 in 4",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/proposition-15-wont-worsen-californias-housing-crisis",
+        image: "images/split-roll_1-768x356.jpg",
+        title: "Split Roll in California",
+        description: "Split Roll tax reform won't make housing worse in California",
+    },
+    {
+        href: "https://urban-institute.medium.com/three-things-we-learned-from-cleaning-one-million-dmv-property-records-4c6fd6ed84fa",
+        image: "images/1_6xAkih_w7mz3Z_hwHOQCDg-268x300.png",
+        title: "Data@Urban Blog",
+        description: "How we used property and zoning data to create housing solutions for the DMV",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/suburbs-arent-under-attack-they-can-be-places-opportunity-all",
+        image: "images/suburbs-768x256.jpg",
+        title: "The Suburbs Aren't Under Attack",
+        description: "What the evidence says about Trump's claim",
+    },
+    {
+        href: "https://www.urban.org/urban-wire/tackling-systemic-barriers-northeast-buffalo-help-break-cycles-poverty",
+        image: "images/gettyimages-594831019_crop-300x159.jpg",
+        title: "Barriers in Northeast Buffalo",
+        description: "Urban Wire blog: families in Buffalo face barriers to oppportunity",
+    },
+    {
+        href: "/Docs/HardPart.pdf",
+        image: "images/IMG_9010-150x150.jpg",
+        title: "Hard Part",
+        description: "A piece about change",
+    },
+    {
+        href: "/Docs/BytheSea.pdf",
+        image: "images/IMG_1995-150x150.jpg",
+        title: "By the Sea",
+        description: "My piece in Davidson's literary magazine, 'Hobart Park'"
+    },
+    {
+        href: "/Docs/UltimateMoment.pdf",
+        image: "images/frisbee.jpg",
+        title: "The Ultimate Moment",
+        description: "On Ultimate Frisbee"
+    },
+]
+
+//set up meta array
+var siteData = 
+    {
+    "portfolio": portfolio,
+    "writing": writing,
+}
+
+
+$(document).ready(function()  {
+
+    $(".content").hide()
+
+    function initializeCarousel (clickedId) {
+        var sliderInitialized = $(".carousel-"+clickedId).data("initialized");
+        console.log(sliderInitialized)
+        // getAllCards(clickedId);
+        if (sliderInitialized == "FALSE") {
+            //if its portfolio or writing
+            if(clickedId == "portfolio" | clickedId == "writing") {
+                console.log("clicked id == porfolio or writing")
+                $('.carousel-'+clickedId).slick({
+                    slidesPerRow: 3,
+                    slidesToScroll: 1,
+                    rows: 2,
+                    dots: true,
+                    arrows: false,
+                    responsive: [
+                        {
+                        breakpoint: 750,
+                        settings: {
+                            slidesPerRow: 1,
+                            rows: 2,
+                            slidesToShow: 1,
+                        }
+                        }
+                    ]
+                });
+                console.log("initialized carousel");
+                $(".carousel-"+clickedId).data("initialized", "TRUE");
+                setHeight();
+            }
+            //if its patrick
+            if(clickedId == "patrick") {
+                console.log("clicked id == patrick")
+                $('.carousel-patrick').slick({
+                    slidesPerRow: 1,
+                    slidesToScroll: 1,
+                    rows: 1,
+                    dots: false,
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                });
+                console.log("initialized carousel");
+                $(".carousel-"+clickedId).data("initialized", "TRUE");
+            }
+        } 
+    };
+
+    function setHeight() {
+        var h = 0;
+        $('.thumbnail').each(function() {
+            if ($(this).height() > h) {
+                h = $(this).height();
+            }
+        });
+        $('.thumbnail').each(function() {
+            $(this).height(h);
+        });
+    }
+
+
+    function reset () {
+        firstClick = ""
+        secondClick = "" 
+        console.log("reset")
+    };
+
+    function openSection (clickedId) {
+        $("#"+clickedId+"-content").show()
+        //show content with matching id
+        $(".row").addClass("row-shrunk")
+        //shrink all main rows
+        //if portfolio or writing
+        initializeCarousel(clickedId)
+        //if patrick
+
+        //initialize carousel
+        console.log("opened "+clickedId)
+        //initialize slick here
+    };
+
+    function closeSection (clickedId) {
+        //hide content with matching id
+        // unInitializeCarousel(clickedId);
+        $(".content").hide();
+        $(".row").removeClass("row-shrunk")
+        console.log("closed "+clickedId)
+    };
+
+    reset();
+
+    $(".row").on("click", function() {
+        if (firstClick == "") { //open the clicked section
+            firstClick = $(this).attr("id");
+            openSection(firstClick);
+        }
+        else if (firstClick != "") { //if a section is open
+            secondClick = $(this).attr("id"); //grab value for section clicked
+            if (firstClick == secondClick) { //if click the open section, reset to main page
+                closeSection(firstClick);
+                reset();
+            }
+            else if(firstClick != secondClick) { //if click a different section, close the open section and open a new section
+                closeSection(firstClick);
+                openSection(secondClick);
+                firstClick = secondClick;
+                secondClick = ""
+            }
+        }
+    });
+
+    //Get all cards function
+    // pass it an argument for portfolio or writing array
+    // Step 1: Iterate through array (forEach)
+    // Step 2: For each object create a HTML card with their data inside
+    // Step 3: Append the newly-created HTML to the DOM (.append)
+    function getAllCards(carouselName) {
+        // Step 1: Iterate through students array (forEach)
+        // Step 2: For each student object create a HTML card with their data inside
+        // Step 3: Append the newly-created HTML to the DOM (.append)
+        console.log("Called getallcards function for " + carouselName)
+        siteData[carouselName].forEach(function(e, i) {
+          var cardHtml = `
+          <div class="thumbnail">
+            <a class = "thumbnail-pic" href="${siteData[carouselName][i].href}" target="_blank" style="background-image:url(${siteData[carouselName][i].image})">
+            </a>
+            <div >
+                <h2>${siteData[carouselName][i].title}</h2>
+                    <p>${siteData[carouselName][i].description}</p>
+            </div>
+          </div>
+          `;
+          // Add newly-created HTML to the DOM
+          $(".carousel-"+carouselName).append(cardHtml);
+          console.log("Added card to DOM");
+        });
+      };
+    //call function for Portfolio
+    //run function for writing
+    getAllCards("portfolio")
+    getAllCards("writing")
+
+
+
+
+});
---
+++ js/masonry.js
@@ -0,0 +1,13 @@
+$(document).ready(function()  {
+    
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.grid-sizer'
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry();
+    });  
+
+});
